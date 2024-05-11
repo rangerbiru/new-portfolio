a34df54a7ccc19ad86b16f6a6599fd1e29718c4b
rangerbiru <rangerbiru.id@gmail.com>
2024-05-11 10:13:09 +0700

    v1.0

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -35,26 +35,26 @@ const Profile = () => {
             </svg>
           </a>
 
-          <a
-            href="https://web.facebook.com/jsishsgxybsiwowjwidhe"
-            target="_blank"
-          >
+          <a href="https://www.fiverr.com/arrafiseptiawan" target="_blank">
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              id="facebook"
               className="w-11 h-11 rounded-md bg-gray-100 p-2 border-white border-2 dark:border-black dark:bg-white hover:border-black transition-all duration-300"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 400 400"
             >
-              <path
-                fill="#1976D2"
-                fill-rule="evenodd"
-                d="M12 5.5H9v-2a1 1 0 0 1 1-1h1V0H9a3 3 0 0 0-3 3v2.5H4V8h2v8h3V8h2l1-2.5z"
-                clip-rule="evenodd"
-              ></path>
+              <circle cx="200" cy="200" fill="#00b22d" r="200" />
+              <g fill="#fff">
+                <path d="M364.4 162.7c0-6.6-5.2-12-11.8-12-6.4 0-11.7 5.3-11.7 12 0 6.6 5.2 12 11.7 12 6.6.1 11.8-5.3 11.8-12zm-11.8 8.7c-4.5 0-8-3.8-8-8.7 0-4.8 3.5-8.6 8-8.6 4.6 0 8.2 3.8 8.2 8.6 0 4.9-3.6 8.7-8.2 8.7z" />
+                <path d="M355.8 163.7c.6-.2 1.9-1.1 1.9-3 0-2.3-1.5-3.7-4-3.7h-5.3v11.3h3.5v-3.8h.9l1.6 3.8h3.8l-2.1-3.9c-.2-.6-.3-.7-.3-.7zm-3-1.6h-.9v-2.7h.9c.8 0 1.2.4 1.2 1.3.1.9-.4 1.4-1.2 1.4z" />
+                <circle cx="104.6" cy="163" r="9" />
+                <path d="M114 177.9H72.8v-2.7c0-5.3 5.3-5.4 8-5.4 3.1 0 4.5.3 4.5.3v-14.6s-2.8-.4-6.6-.4c-8.6 0-24.5 2.4-24.5 20.6v2.3h-7.5v13.5h7.5V220h-7v13.5H81V220h-8.2v-28.5h22.5V220h-7v13.5H121V220h-7zm70 0h-29.5v13.5h5l-6.4 20c-1.2 3.3-1.5 7.3-1.5 7.3h-.4s-.3-4-1.5-7.3l-6.4-20h5v-13.5h-29.5v13.5h6.2l15.4 42h22l15.4-42h6.2zm54.6 25.5c0-15.4-9.3-26.8-25.8-26.8-17.9 0-28.9 12.7-28.9 29 0 14.8 10.7 29.1 30.5 29.1 15 0 23.9-7.8 23.9-7.8l-6.8-12.9s-7.4 5.3-15.6 5.3c-5.9 0-11.5-3.1-12.9-10.2h35.2c-.1-.1.4-3.9.4-5.7zm-35.2-4.6c1-4.3 3.6-8.2 8.9-8.2 4.1 0 7 3.8 7 8.2zm114.1-8.1h-.2s.2-1.1.2-2.8V185c0-5.1-2.8-7.1-7.9-7.1h-17.5v13.5h5.2c1.5 0 2.4.9 2.4 2.4V220h-7.5v13.5h33.7V220h-7.5v-8.1c0-10.1 5-16.7 15.3-16.7 2.3 0 3.8.3 3.8.3v-18.3s-1.1-.2-2.2-.2c-8.4-.1-15.4 6.1-17.8 13.7zm-49.3 0h-.2s.2-1.1.2-2.8V185c0-5.1-2.8-7.1-7.9-7.1h-17.5v13.5h5.2c1.5 0 2.4.9 2.4 2.4V220h-7.5v13.5h33.7V220h-7.5v-8.1c0-10.1 5-16.7 15.3-16.7 2.3 0 3.8.3 3.8.3v-18.3s-1.1-.2-2.2-.2c-8.5-.1-15.5 6.1-17.8 13.7z" />
+              </g>
             </svg>
           </a>
 
-          <a href="#" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/muhammad-arrafi-septiawan-2619b3224/"
+            target="_blank"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 256 256"
@@ -71,7 +71,7 @@ const Profile = () => {
             </svg>
           </a>
 
-          <a href="#" target="_blank">
+          <a href="https://www.instagram.com/arrafi_septiawan/" target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
@@ -209,7 +209,7 @@ const Profile = () => {
           <div className="text-phone">
             <h2 className="font-normal dark:text-black text-sm">Location</h2>
             <p className="font-semibold dark:text-black text-base">
-              Cikarang Utara
+              Cikarang, Indonesia
             </p>
           </div>
         </div>
@@ -286,10 +286,11 @@ const Profile = () => {
         </div>
       </div>
 
-      <div className="flex flex-col items-center mt-8">
+      <div className="flex flex-col items-center mt-8 max-lg:mt-6">
         <a
-          href="#"
-          className="flex items-center gap-1 bg-blue-600 py-2 px-4 rounded-lg text-white"
+          href="https://rangerbiru.github.io/cvOri2/index.html"
+          className="flex items-center gap-1 bg-blue-600 py-2 px-4 rounded-lg text-white max-lg:w-full max-lg:justify-center max-lg:py-2.5"
+          target="_blank"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -302,13 +303,7 @@ const Profile = () => {
               d="M21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Zm-9.71,1.71a1,1,0,0,0,.33.21.94.94,0,0,0,.76,0,1,1,0,0,0,.33-.21l4-4a1,1,0,0,0-1.42-1.42L13,12.59V3a1,1,0,0,0-2,0v9.59l-2.29-2.3a1,1,0,1,0-1.42,1.42Z"
             ></path>
           </svg>
-          <a
-            href="https://rangerbiru.github.io/cvOri2/index.html"
-            target="_blank"
-            className="font-medium"
-          >
-            Download CV
-          </a>
+          Download CV
         </a>
       </div>
     </div>
